f8578bcacd1cbfbba318eff36ff8ea0d95679816
manu-devops98 <manuelcappello7@gmail.com>
2022-02-23 18:43:19 +0100

    add layout route index

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -12,46 +12,46 @@
         <div class="row">
             <div class="col">
                 <form action="{{ route('comics.store') }}" method="post">
-            @csrf
-            @method('POST')
-            <div class="mb-3">
-              <label for="title" class="form-label">Title</label>
-              <input type="text" class="form-control" id="title" name="title">
-            </div>
-            <div class="mb-3">
-              <label for="genre" class="form-label">Genre</label>
-              <input type="text" class="form-control" id="genre" name="genre">
-            </div>
-            <div class="mb-3">
-                <label for="authors" class="form-label">Authors</label>
-                <input type="text" class="form-control" id="authors" name="authors" rows="3">
-            </div>
-            <div class="mb-3">
-              <label for="writers" class="form-label">Writers</label>
-              <input type="text" class="form-control" id="writers" name="writers">
-            </div>
-            <div class="mb-3">
-              <label for="edition" class="form-label">Edition</label>
-              <input type="text" class="form-control" id="edition" name="edition">
-            </div>
-            <div class="mb-3">
-              <label for="publisher" class="form-label">Publisher</label>
-              <input type="text" class="form-control" id="publisher" name="publisher">
-            </div>
-            <div class="mb-3">
-              <label for="isbn" class="form-label">ISBN</label>
-              <input type="text" class="form-control" id="isbn" name="isbn">
-            </div>
-            <div class="mb-3">
-              <label for="photo" class="form-label">Photo</label>
-              <input type="text" class="form-control" id="photo" name="photo">
-            </div>
-            <div class="mb-3">
-              <label for="price" class="form-label">Price</label>
-              <input type="number" class="form-control" id="price" name="price">
-            </div>
-            
-            <button type="submit" class="btn btn-danger">Save</button>
+                @csrf
+                @method('POST')
+                <div class="mb-3">
+                  <label for="title" class="form-label">Title</label>
+                  <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="mb-3">
+                  <label for="genre" class="form-label">Genre</label>
+                  <input type="text" class="form-control" id="genre" name="genre">
+                </div>
+                <div class="mb-3">
+                    <label for="authors" class="form-label">Authors</label>
+                    <input type="text" class="form-control" id="authors" name="authors" rows="3">
+                </div>
+                <div class="mb-3">
+                  <label for="writers" class="form-label">Writers</label>
+                  <input type="text" class="form-control" id="writers" name="writers">
+                </div>
+                <div class="mb-3">
+                  <label for="edition" class="form-label">Edition</label>
+                  <input type="text" class="form-control" id="edition" name="edition">
+                </div>
+                <div class="mb-3">
+                  <label for="publisher" class="form-label">Publisher</label>
+                  <input type="text" class="form-control" id="publisher" name="publisher">
+                </div>
+                <div class="mb-3">
+                  <label for="isbn" class="form-label">ISBN</label>
+                  <input type="text" class="form-control" id="isbn" name="isbn">
+                </div>
+                <div class="mb-3">
+                  <label for="photo" class="form-label">Photo</label>
+                  <input type="text" class="form-control" id="photo" name="photo">
+                </div>
+                <div class="mb-3">
+                  <label for="price" class="form-label">Price</label>
+                  <input type="number" class="form-control" id="price" name="price">
+                </div>
+                
+                <button type="submit" class="btn btn-danger">Save</button>
           </form>
             </div>
         </div>
---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.base')
+
+@section('documentTitle')
+    {{ $title }}
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1>Admin Books</h1>
+            <div class="col">
+                <a href="{{ route('comics.create') }}" class="btn btn-danger mb-5">Aggiungi libro</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <table class="table table-primary">
+                    <thead>
+                        <tr class="table-primary">
+                            <th>Title</th>
+                            <th>Authors</th>
+                            <th>Genre</th>
+                            <th>Edition</th>
+                            <th>Price</th>
+                            <th>ISBN</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($books as $book)
+                        <tr>
+                            <td>{{ $book->title }}</td>
+                            <td>{{ $book->authors }}</td>
+                            <td>{{ $book->genre }} </td>
+                            <td>{{ $book->edition }} </td>
+                            <td>{{ $book->price }} €</td>
+                            <td>{{ $book->ISBN }} </td>
+                            <td><a class="btn btn-primary" href="{{ route('comics.show', $book) }}">View</a></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                {{ $books->links() }}
+            </div>
+        </div>
+@endsection
